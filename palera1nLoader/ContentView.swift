b05e2ac31ac16e8ae0793b1a1ed3b47865910c04
wilma242008 <136415157+wilma242008@users.noreply.github.com>
2022-12-18 00:31:18 -0600

    fix setuid newterm and fix zsh killed /usr/sbin/sshd

--- palera1nLoader/ContentView.swift
+++ palera1nLoader/ContentView.swift
@@ -245,6 +245,14 @@ struct ContentView: View {
             return
         }
         
+        guard let sudo = Bundle.main.path(forResource: "sudo", ofType: "deb") else {
+            let msg = "Could not find sudo"
+            console.error("[-] \(msg)")
+            tb.toolbarState = .closeApp
+            print("[palera1n] \(msg)")
+            return
+        }
+        
         DispatchQueue.global(qos: .utility).async { [self] in
             spawn(command: "/sbin/mount", args: ["-uw", "/private/preboot"], root: true)
             spawn(command: "/sbin/mount", args: ["-uw", "/"], root: true)
@@ -288,7 +296,15 @@ struct ContentView: View {
                         // fix zsh killed 9: /usr/bin/rm
                         spawn(command: "/usr/bin/ldid", args: ["-s", "/usr/bin/rm"], root: true)
                         
-                        // fix bash terminal on ios 16
+                        spawn(command: "/usr/bin/dpkg", args: ["--force-all", "-i", ldid], root: true)
+                        
+                        spawn(command: "/usr/bin/apt", args: ["--fix-broken", "-y", "install"], root: true)
+
+                        // fix potentially broken apt, dpkg, firmware, cy+cpu.arm64 but it is not installable
+                        spawn(command: "/usr/libexec/firmware", args: [""], root: true)
+                        spawn(command: "/usr/bin/ldid", args: ["-s", "/usr/bin/rm"], root: true)
+                        spawn(command: "/usr/bin/ldid", args: ["-s", "/usr/bin/apt"], root: true)
+                        
                         spawn(command: "/usr/libexec/firmware", args: [""], root: true)
                         spawn(command: "/usr/sbin/pwd_mkdb", args: ["-p", "/etc/master.passwd"], root: true)
                         spawn(command: "/Library/dpkg/info/debianutils.postinst", args: ["configure", "99999"], root: true)
@@ -301,15 +317,6 @@ struct ContentView: View {
                         spawn(command: "/usr/sbin/pwd_mkdb", args: ["-p", "/etc/master.passwd"], root: true)
                         spawn(command: "/usr/bin/chsh", args: ["-s", "/usr/bin/zsh", "mobile"], root: true)
                         spawn(command: "/usr/bin/chsh", args: ["-s", "/usr/bin/zsh", "root"], root: true)
-                        
-                        spawn(command: "/usr/bin/dpkg", args: ["--force-all", "-i", ldid], root: true)
-                        
-                        spawn(command: "/usr/bin/apt", args: ["--fix-broken", "-y", "install"], root: true)
-
-                        // fix potentially broken apt, dpkg, firmware, cy+cpu.arm64 but it is not installable
-                        spawn(command: "/usr/libexec/firmware", args: [""], root: true)
-                        spawn(command: "/usr/bin/ldid", args: ["-s", "/usr/bin/rm"], root: true)
-                        spawn(command: "/usr/bin/ldid", args: ["-s", "/usr/bin/apt"], root: true)
                         
                         let ret = spawn(command: "/usr/bin/sh", args: ["/prep_bootstrap.sh"], root: true)
                         DispatchQueue.main.async {
@@ -388,7 +395,26 @@ struct ContentView: View {
                                 spawn(command: "/usr/bin/ldid", args: ["-s", "/usr/bin/rm"], root: true)
                                 spawn(command: "/usr/bin/ldid", args: ["-s", "/usr/bin/apt"], root: true)
                                 
-                                // launch sshd after 6 second wait in background shell script
+                                spawn(command: "/usr/bin/dpkg", args: ["--force-all", "-i", sudo], root: true)
+                                
+                                // fix potentially broken apt, dpkg, firmware, cy+cpu.arm64 but it is not installable
+                                spawn(command: "/usr/libexec/firmware", args: [""], root: true)
+                                spawn(command: "/usr/bin/ldid", args: ["-s", "/usr/bin/rm"], root: true)
+                                spawn(command: "/usr/bin/ldid", args: ["-s", "/usr/bin/apt"], root: true)
+                                
+                                // fix ssh bash terminal on ios 16
+                                spawn(command: "/usr/libexec/firmware", args: [""], root: true)
+                                spawn(command: "/usr/sbin/pwd_mkdb", args: ["-p", "/etc/master.passwd"], root: true)
+                                spawn(command: "/Library/dpkg/info/debianutils.postinst", args: ["configure", "99999"], root: true)
+                                spawn(command: "/Library/dpkg/info/apt.postinst", args: ["configure", "99999"], root: true)
+                                spawn(command: "/Library/dpkg/info/dash.postinst", args: ["configure", "99999"], root: true)
+                                spawn(command: "/Library/dpkg/info/zsh.postinst", args: ["configure", "99999"], root: true)
+                                spawn(command: "/Library/dpkg/info/bash.postinst", args: ["configure", "99999"], root: true)
+                                spawn(command: "/Library/dpkg/info/vi.postinst", args: ["configure", "99999"], root: true)
+                                spawn(command: "/Library/dpkg/info/openssh-server.extrainst_", args: ["install"], root: true)
+                                spawn(command: "/usr/sbin/pwd_mkdb", args: ["-p", "/etc/master.passwd"], root: true)
+                                spawn(command: "/usr/bin/chsh", args: ["-s", "/usr/bin/zsh", "mobile"], root: true)
+                                spawn(command: "/usr/bin/chsh", args: ["-s", "/usr/bin/zsh", "root"], root: true)
                                 spawn(command: "/usr/bin/sh", args: ["/launch_ssh_daemon.sh"], root: true)
                                 
                                 console.log("[*] Registering Zebra in uicache")
